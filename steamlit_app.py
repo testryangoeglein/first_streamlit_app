42dc42cf44d2a7822f02869db02e1e270aa6d191
testryangoeglein <140354388+testryangoeglein@users.noreply.github.com>
2023-07-23 16:34:32 -0700

    Update steamlit_app.py
    type header 1 update

--- steamlit_app.py
+++ steamlit_app.py
@@ -2,7 +2,7 @@ import streamlit
 
 streamlit.title('My Parents New Healthy Diner')
 
-streamlit.header('Breakfast Favosrite')
+streamlit.header('Breakfast Favorites')
 streamlit.text('🥗 Kale, Spinach & Rocket Smoothie')
 streamlit.text('🐔 Hard-Boiled Free-Range Egg')
 streamlit.text('🥑 🍞 Avocado Toast')
