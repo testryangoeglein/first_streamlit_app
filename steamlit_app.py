a8a22ecc0e452ed624fe0f6fc58cb112fc188bcb
testryangoeglein <140354388+testryangoeglein@users.noreply.github.com>
2023-07-24 17:11:12 -0700

    Update steamlit_app.py

--- steamlit_app.py
+++ steamlit_app.py
@@ -44,6 +44,7 @@ streamlit.dataframe(my_data_rows)
 
 # all the end user to add fruit to the list
 add_my_fruit = streamlit.text_input('What fruit would you like to add?','Kiwi')
-streamlit.write('The user entered ', fruit_choice)
-my_cur.execute("insert into fruit_load_list values ('add_my_fruit')")
+streamlit.write('Thanks for adding ', my_fruit_choice)
+my_cur.execute("insert into fruit_load_list values ('" + add_my_fruit + "')")
 
+my_cur.execute("insert into fruit_load_list values ('from streamlit')")
